9180a099a2766220042fd8b003cc517c7e1a1db8
hiro08gh <hiro08gh@gmail.com>
2020-02-03 22:12:30 +0900

    add redux-devtools

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,13 @@
-import { createStore } from 'redux';
-import { reducer } from './reducers';
+import { applyMiddleware, createStore, compose } from 'redux';
+import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './reducers/rootReducer';
 
-const store = createStore(reducer, composeWithDevTools());
+const dev = process.env.NODE_ENV === 'development';
+
+const store = createStore(
+  rootReducer,
+  dev ? composeWithDevTools(applyMiddleware(thunk)) : compose(applyMiddleware(thunk))
+);
 
 export default store;
