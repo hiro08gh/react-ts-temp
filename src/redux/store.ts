f8beb694dc5a455a768a7a7052183d11296746bc
hiro08gh <hiro08gh@gmail.com>
2020-02-05 11:23:37 +0900

    add redux-sagaの導入

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,24 @@
 import { applyMiddleware, createStore, compose } from 'redux';
 import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducers/rootReducer';
 
+function* Saga() {
+  console.log('Hello Sagas!');
+}
+
 const dev = process.env.NODE_ENV === 'development';
+
+const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
   rootReducer,
-  dev ? composeWithDevTools(applyMiddleware(thunk)) : compose(applyMiddleware(thunk))
+  dev
+    ? composeWithDevTools(applyMiddleware(thunk, sagaMiddleware))
+    : compose(applyMiddleware(thunk, sagaMiddleware))
 );
+
+sagaMiddleware.run(Saga);
 
 export default store;
